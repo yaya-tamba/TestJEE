002324616a5e08a689db2619b1778863bc3aa90a
Tamba Yaya <yaaztamba@gmail.com>
2019-09-26 15:49:58 +0100

    fist commit

---
+++ src/java/com/src/jpa/Client.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.src.jpa;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author asus
+ */
+@Entity
+@Table(name = "client", catalog = "bdd", schema = "")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Client.findAll", query = "SELECT c FROM Client c")
+    , @NamedQuery(name = "Client.findByPrenom", query = "SELECT c FROM Client c WHERE c.prenom = :prenom")
+    , @NamedQuery(name = "Client.findByNom", query = "SELECT c FROM Client c WHERE c.nom = :nom")})
+public class Client implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "Prenom", nullable = false, length = 20)
+    private String prenom;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "Nom", nullable = false, length = 20)
+    private String nom;
+
+    public Client() {
+    }
+
+    public Client(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Client(String prenom, String nom) {
+        this.prenom = prenom;
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (prenom != null ? prenom.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Client)) {
+            return false;
+        }
+        Client other = (Client) object;
+        if ((this.prenom == null && other.prenom != null) || (this.prenom != null && !this.prenom.equals(other.prenom))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.src.jpa.Client[ prenom=" + prenom + " ]";
+    }
+    
+}
---
+++ src/java/com/src/session/ClientFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.src.session;
+
+import com.src.jpa.Client;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author asus
+ */
+@Stateless
+public class ClientFacade extends AbstractFacade<Client> {
+
+    @PersistenceContext(unitName = "M_moireJSFPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ClientFacade() {
+        super(Client.class);
+    }
+    
+}
